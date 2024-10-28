0f09d82503525b9fe8b974d344755271fdcf0be7
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2024-10-29 00:08:53 +0300

    added avatar photo for user and cover for it

--- lib/view/HomePageNavBar.dart
+++ lib/view/HomePageNavBar.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePageNavBar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        UserAccountsDrawerHeader(
+        accountName: Text("Youssef Mahmoud Ahmed"),
+        accountEmail: Text("youssef@gmail.com"),
+        currentAccountPicture: CircleAvatar(
+            child: ClipOval(
+              child: SvgPicture.asset("assets/male_avatar.svg",fit: BoxFit.cover,),
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.purple,
+          image:DecorationImage(
+            image: AssetImage("assets/menu_cover.jpg"),
+            fit:  BoxFit.cover,
+          )
 
+        ),
+        )
+      ],
+    ),
     );
   }
 
